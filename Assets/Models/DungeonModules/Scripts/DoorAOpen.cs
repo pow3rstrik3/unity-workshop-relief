ae3b697fffb29cc5fda425a8dfeeb973e612943d
Mitch peters <M.peters12@student.han.nl>
2019-03-12 16:26:34 +0100

    door added

---
+++ Assets/Models/DungeonModules/Scripts/DoorAOpen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAOpen : MonoBehaviour
+{
+    PickupController pickupController;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        pickupController = other.GetComponent<PickupController>();
+        if (pickupController.silverKey && pickupController.goldKey)
+        {
+            GetComponent<Animator>().SetTrigger("DoorATrigger");
+        }
+        
+    }
+}
